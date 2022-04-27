b8fbe574c4f38654d733ba2a0b88d0c5bbbc13db
chlwlgns524 <chlwlgns524@naver.com>
2022-04-27 09:37:07 +0900

    2438-ver1

---
+++ 2438.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+/*
+* º° Âï±â - 1
+*/
+int main() {
+	int n;
+
+	scanf("%d", &n);
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++)
+			printf("%c", '*');
+		puts("");
+	}
+
+	return 0;
+}
