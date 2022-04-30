4acff14135d909c6ddde709f953215face1bab68
chlwlgns524 <chlwlgns524@naver.com>
2022-04-30 12:16:12 +0900

    2475-ver1

---
+++ 2475.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+/*
+* °ËÁõ¼ö
+*/
+int main() {
+	int n;
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &n);
+		sum += (n * n);
+	}
+	printf("%d\n", sum % 10);
+
+	return 0;
+}
